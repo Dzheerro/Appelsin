30350fdfbd8ec34306291dea5b1bd916ca774d7d
Mark Bulavin <cloudlet@Marks-MacBook-Pro-2.local>
2024-10-24 15:05:29 +0300

    addition to previous commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { HomeComponent } from './shared/components/home/home.component';
+
+export const routes: Routes = [
+  { path: '', pathMatch: 'full', component: HomeComponent },
+  { path: 'login', loadComponent: () => import('./shared/components/login/login.component').then((c) => c.LoginComponent) },
+  { path: 'signup', loadComponent: () => import('./shared/components/signup/signup.component').then((c) => c.SignupComponent) },
+  { path: '**', redirectTo: '' }
+];
