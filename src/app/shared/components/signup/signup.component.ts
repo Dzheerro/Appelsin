564a1d3aa75d5e0700e7e9ee94e581c5cb8c2376
Dzheerro <mark.bulavin2005@gmail.com>
2024-11-25 02:52:30 +0200

    implemented logic to create user, generated environments for storing apiEndpoint at correct place

--- src/app/shared/components/signup/signup.component.ts
+++ src/app/shared/components/signup/signup.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+import { AuthenticationService } from '../../../core/services/authentication.service';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.scss'
 })
-export class SignupComponent {}
+export class SignupComponent implements OnInit {
+  signUpForm!: FormGroup;
+  profileType = false;
+
+  constructor(
+    private fb: FormBuilder,
+    private readonly authService: AuthenticationService
+  ) {}
+
+  ngOnInit(): void {
+    this.signUpForm = this.fb.group({
+      first_name: ['', Validators.required],
+      last_name: ['', Validators.required],
+      username: ['', Validators.required],
+      email: ['', Validators.email],
+      phone_number: ['', Validators.required],
+      password: ['', Validators.required],
+      profile_type: false
+    });
+  }
+
+  submitForm(): void {
+    if (this.signUpForm.valid) {
+      this.signUpForm.value.profile_type = this.profileType ? 'Artist' : 'Listener';
+    }
+  }
+}
